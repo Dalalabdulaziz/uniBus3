3c88030ccddb9ef931915a667306cdd6907d8a37
دلال الوافي <dlalalwafy@MacBook-Air-alkhas-b-dlal.local>
2018-05-20 02:05:40 +0300

    first commit

---
+++ app/src/main/java/com/example/dlalalwafy/unibus/studentyanbu.java
@@ -0,0 +1,13 @@
+package com.example.dlalalwafy.unibus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class studentyanbu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studentyanbu);
+    }
+}
